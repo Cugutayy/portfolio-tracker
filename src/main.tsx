f26dc1590a928b164f7cd9a65df69b05621a2406
Cugutayy <s.cagatay.sonmez@gmail.com>
2026-09-11 23:38:41 +0300

    style: apply hub visibility overrides

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { KediDexGuard } from './components/KediDexGuard'
 import './styles/globals.css'
+import './styles/hub-visibility.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
